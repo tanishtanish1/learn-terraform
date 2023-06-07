93aaa46ccf6ad0c7995951c5d13f07329263dbdb
tanishtanish1 <siddam.thaneesh@gmail.com>
2023-06-08 09:28:11 +1000

    terra1

--- demo-03/vars.tf
+++ demo-03/vars.tf
@@ -13,3 +13,40 @@ output "sample1" {
 output "sample-txt" {
   value = "value of sample - ${var.sample}"
 }
+
+#plain
+variable "course1" {
+  default = "dev training"
+}
+
+#list
+variable "course2" {
+  default = ["java","python","rubu", "javascript"  ]
+}
+#map
+variable "course3" {
+  default = {
+    devops={
+      name="dev"
+      trainer="rahhu"
+      fee    =20000
+
+  }
+    aws={
+      namee="dev1"
+      trainer="siva"
+      fee    =20000
+    }
+
+  }
+}
+
+output "course1" {
+  value = "var.course1"
+}
+output "course2" {
+  value = "var.course2"
+}
+output "course3" {
+  value = "var.course3"
+}
