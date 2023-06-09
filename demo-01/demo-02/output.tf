bc6c3f114673c866a738bf85695780e8749fda3c
tanishtanish1 <siddam.thaneesh@gmail.com>
2023-06-09 10:27:50 +1000

    terra1

--- demo-01/demo-02/output.tf
+++
@@ -1,6 +0,0 @@
-output "sample1" {
-  value = "Hello world"
-}
-output "sample2" {
-  value = "1000"
-}
--- demo-01/output-01.tf
+++ demo-01/output-01.tf
@@ -1,6 +1,8 @@
-output "sample1" {
-  value = "hellow world"
-}
-output "sample2" {
-  value = "1000"
+resource "aws_instance" "web" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
