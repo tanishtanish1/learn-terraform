4daeea20081a46b565885d9d57e4b0b029544e3a
tanishtanish1 <siddam.thaneesh@gmail.com>
2023-07-04 18:59:59 +1000

    ansible1

--- vpc/main.tf
+++ vpc/main.tf
@@ -36,3 +36,24 @@ resource "aws_subnet" "public-subnet" {
 
 }
 
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "timing-1-public-routetable"
+    Terraform= "true"
+    Environment= "Dev"
+  }
+
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public-subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
