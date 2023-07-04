be10a19a26e23aa09c6397d12be20736a16819ae
tanishtanish1 <siddam.thaneesh@gmail.com>
2023-07-04 18:42:44 +1000

    ansible1

--- vpc/main.tf
+++ vpc/main.tf
@@ -24,3 +24,15 @@ resource "aws_vpc" "main" {
   }
 }
 
+resource "aws_subnet" "public-subnet" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    Name = "timing-1-public subnet"
+    Terraform= "true"
+    Environment= "Dev"
+  }
+
+
+}
+
