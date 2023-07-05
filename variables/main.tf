ef2820fba1fb0e8b273dc121344dad3f2093cc91
tanishtanish1 <siddam.thaneesh@gmail.com>
2023-07-05 10:35:02 +1000

    ansible1

--- variables/main.tf
+++ variables/main.tf
@@ -5,3 +5,28 @@ resource "aws_vpc" "main" {
   enable_dns_hostnames = var.enable_dns_hostnames
   tags                 = var.tags
 }
+# security group for postgress RDS, 5432
+resource "aws_security_group" "allow_postgress" {
+  name        = "allow_postgress"
+  description = "allow_postgress inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = var.postgress_port
+    to_port          = var.postgress_port
+    protocol         = "tcp"
+    cidr_blocks      = var.cidr_list
+
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = var.tags
+  }
--- variables/variables.tf
+++ variables/variables.tf
@@ -22,3 +22,11 @@ variable "tags" {
     Environment= "Dev"
   }
 }
+variable "postgress_port" {
+  type = number
+  default = 5432
+}
+variable "cidr_list" {
+  type = list
+  default = [ "10.0.1.0/24","10.0.2.0/24 "]
+}
